f38c1206e0c57ff27126038dd5e24740ec641696
Yan <yan.tong@univ.ox.ac.uk>
2017-06-01 15:42:14 +0100

    Deleted unneccessary inputs and linearization.

--- SysMtxSingle.m
+++ SysMtxSingle.m
@@ -1,4 +1,4 @@
-function DA = SysMtxSingle(k,b0,b1,mask,dt,tb0,Positions)
+function DA = SysMtxSingle(k,b0,b1,dt,tb0,Positions)
 %for single channel only now!
 Nt = size(k,1);
 Ns = numel(b1);
@@ -10,9 +10,7 @@ GAMMA = 42.577E6; % in Hz/T
 m0 = 1;
 AA = zeros(Ns, Nt);
 Positions(3,:,:) = [];
-Positions = Positions(:,:).';
-Positions = Positions(mask,:);
-Positions = conj(Positions);
+
 for ii = 1:Ns
     for jj = 1:Nt
         AA(ii,jj) = (1i*GAMMA*dt*m0) * exp(1i*GAMMA*b0(ii)*(tb0(jj)-PulseDuration)) ...
