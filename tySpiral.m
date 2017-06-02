fb2e3898c22b2824c9fa8719738670d9b548df8f
Yan <yan.tong@univ.ox.ac.uk>
2017-06-02 13:25:09 +0100

    tested the first section (on data loading)

--- tySpiral.m
+++ tySpiral.m
@@ -1,19 +1,16 @@
 %addpath(genpath('/Users/ytong/Documents/MATLAB/WTCpTx'));
 singleTxPath = '/Users/ytong/Documents/MATLAB/20170516_F7T_2017_PH_038';
 %singleTxPath = '/Volumes/Data/DICOM/2017-05/20170516_F7T_2017_PH_038';
-singleTxObj = DicomFM.WTCSingleTxFieldmaps(singleTxPath);
-singleTxObj.interpolateTo('B1');
-singleTxObj.createMask(30,false);
-B1 = singleTxObj.getB1('Gauss','delete',[]);
-B0 = singleTxObj.getB0('Hz','delete');
-Loc = singleTxObj.getLoc('delete');
-%fov = [25.6 25.6];
-[dimb1(1),dimb1(2), ~] = size(WTCmask);
-[DimX, DimY, NumSlices] = size(B0);clc
-Positions = singleTxObj.getPositions('cm','delete');
-WTCmask = singleTxObj.getMask;
-Ns = size(B1,1);
-fov = 25.6; %fov is hard coded at this momemnt. In cm.
+    singleTxObj = DicomFM.WTCSingleTxFieldmaps(singleTxPath);
+    singleTxObj.interpolateTo('B1');
+    singleTxObj.createMask(30,false);
+    slice = round(size(singleTxObj.getMask([]),3))/2;
+    mask = singleTxObj.getMask(slice);
+    B1 = singleTxObj.getB1('Gauss','delete',slice);
+    B0 = singleTxObj.getB1('Hz','delete',slice);
+    Positions = singleTxObj.getPositions('cm','delete',slice);
+    Ns = size(B1,1);
+    %fov = 25.6; %fov is hard coded at this momemnt. In cm.
 %%
 %Get k-space trajectory
   traj = 'spiral';
