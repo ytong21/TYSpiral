7a163ae59a51f08482056375271b7b787de2fc8d
Yan <yan.tong@univ.ox.ac.uk>
2017-07-03 14:14:07 +0100

    Changed pusle duration and how excitation pattern is created.

--- tySpiral.m
+++ tySpiral.m
@@ -1,22 +1,28 @@
 %addpath(genpath('/Users/ytong/Documents/MATLAB/WTCpTx'));
 
 %singleTxPath = '/Users/ytong/Documents/MATLAB/20170516_F7T_2017_PH_054';
-singleTxPath = '/Volumes/Data/DICOM/2017-06/20170613_F7T_2017_PH_054';
+singleTxPath = '/Volumes/Data/DICOM/2017-07/20170703_F7T_2017_PH_065';
     singleTxObj = DicomFM.WTCSingleTxFieldmaps(singleTxPath);
     singleTxObj.interpolateTo('B1');
     singleTxObj.createMask(@DicomFM.maskFunctions.multithreshMask,false);
+    
+    
     slice = round(size(singleTxObj.getMask,3))/2;
     singleTxObj.setSlice(slice);
+    singleTxObj.plotSlice(slice,3);
     mask = singleTxObj.getMask;
     b1 = singleTxObj.getB1PerV('Hz','delete');
     b0 = singleTxObj.getB0('Hz','delete');
     Positions = singleTxObj.getPositions('cm','delete'); %3 by Ns matrix
     Positions = Positions';%SysMtxSingle takes Ns by 3 matrix.
     Ns = size(b1,1);
+    
+    
    
 %%
 %Get k-space trajectory
-deltax = 15/50; % spatial resolution of trajectory
+deltax = 10/50; % spatial resolution of trajectory
+% Modified. Originally 15/50.
 FOVsubsamp = 10; % cm, xfov of spiral
 densamp = 75; % duration of full density sampling (# of samples)
 dentrans = 75; % duration of transition from higher to lower
@@ -53,46 +59,57 @@ RampUp = 42.57E6*[real(g(1:lenghtRampUp)) imag(g(1:lenghtRampUp))];% Hz/T
 gOut = [RampUp;grad];%Adding gradient ramp.
 gOut = gOut/425.8; %from Hz/cm to mT/m
 %% Load excitation pattern and center it around the mask
-  load /Users/ytong/Documents/MATLAB/Utility/Pattern/OffRect.mat
-  dim = size(d);
-  
-  [xi,yi] = ndgrid(-dim(1)/2:dim(1)/2-1,-dim(2)/2:dim(2)/2-1);
-  xmi = round(mean(xi(logical(mask(:)))));
-  ymi = round(mean(yi(logical(mask(:)))));
-  %d = circshift(d,[xmi ymi]); %Center the excitation pattern
+%   load OffRect.mat
+%   dim = size(mask);
+%   
+%   [xi,yi] = ndgrid(-dim(1)/2:dim(1)/2-1,-dim(2)/2:dim(2)/2-1);
+%   xmi = round(mean(xi(logical(mask(:)))));
+%   ymi = round(mean(yi(logical(mask(:)))));
+%   %d = circshift(d,[xmi ymi]); %Center the excitation pattern
+  dim = size(mask);
+  d = zeros(dim);
+  d(round(0.35*dim(1)):round(0.5*dim(1)),round(0.25*dim(2)):round(0.65*dim(2))) = 1;
   d = ift2((hamming(dim(1))*hamming(dim(2))').^2.*ft2(d));%Smooth it a bit.
-  
 % %% Build RF waveform roughness penalty matrix 
     R = sqrt(roughbeta)*spdiags([-ones(Nt,1) ones(Nt,1)],[0 1],Nt,Nt) + ...
         sqrt(roughbeta)*speye(Nt);
     Rfull = kron(speye(Nc),R);
 % 
   %% Create pulse
-tikhonov = 2e-5;
+tikhonov = 1e-5;
 [RFOut,finalCost,finalPwr,finalMag] = CG_SysMat(DA,d(mask(:)),tikhonov,mask);
 fprintf('Max RF = %f\n',max(abs(RFOut)))
   % Plotting
-  magnetization = make_blochSim(RFOut,b1,b0,grad,dt,Positions,mask);
+%   RFOut = [0 ;RFOut];
+%   RFOut(end) = [];
+  magnetization = make_blochSim(RFOut,b1,b0,gOut*425.8,dt,Positions,mask);
   m = zeros(dim);m(mask) = DA*RFOut(:);
   err = norm(m-d)/norm(d);
   roughness = norm(Rfull*RFOut(:));
   pow = sqrt(mean(abs(RFOut(:)).^2));
   fprintf('All-channels NRMSE: %0.2f%%. All-channels roughness: %f.\n',err*100,roughness);
-  %%plotting
+  %% plotting
+
   maxamp = max(abs([d(:);m(:)]));
-  figure(2)
+  figure(7)
   subplot(221)
   imagesc(abs(d),[0 maxamp]);axis image;colorbar
   title 'Desired pattern'
   subplot(222)
   imagesc(abs(m),[0 maxamp]);axis image;colorbar
-  title(sprintf('Final pattern',Nc));
+  title(sprintf('Final pattern'));
   subplot(223)
   imagesc(abs(magnetization.mxy),[0 maxamp]);axis image;colorbar
   title(sprintf('Bloch Simulation'));
   subplot(224)
   imagesc(abs(m)-abs(d),[0 maxamp]);axis image;colorbar
   title(sprintf('Magnitude Error\nComplex NRMSE = %0.2f%%',err*100));
-  
+  fprintf('The slice position is %.4f mm.\n',10*Positions(1,3));%Positions in cm.
+  %%
+  figure(1234)
+  clf
+  surf(abs(magnetization.mxy),'edgecolor','none')
+  view([90 90])
+
 %% Writing into text file
   MakeDotH(RFOut,gOut,dt);
