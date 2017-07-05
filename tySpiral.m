faf8fd54fbc4dda738ed5dbcf34f9380854dc318
Yan <yan.tong@univ.ox.ac.uk>
2017-07-05 15:44:36 +0100

    Added while loop for power calculation.

--- tySpiral.m
+++ tySpiral.m
@@ -1,7 +1,7 @@
 %addpath(genpath('/Users/ytong/Documents/MATLAB/WTCpTx'));
 
 %singleTxPath = '/Users/ytong/Documents/MATLAB/20170516_F7T_2017_PH_054';
-    singleTxPath = '/Volumes/Data/DICOM/2017-07/20170703_F7T_2017_PH_065';
+    singleTxPath = '/Volumes/Data/DICOM/2017-07/20170704_F7T_2017_PH_069';
     singleTxObj = DicomFM.WTCSingleTxFieldmaps(singleTxPath);
     singleTxObj.interpolateTo('B1');
     slice = round(size(singleTxObj.getMask,3)/2);
@@ -14,14 +14,43 @@
     Positions = singleTxObj.getPositions('cm','delete'); %3 by Ns matrix
     Positions = Positions';%SysMtxSingle takes Ns by 3 matrix.
     Ns = size(b1,1);
-    
-    
-   
-%%
-%Get k-space trajectory
-deltax = 45/50; % spatial resolution of trajectory
+%% Create the excitation pattern
+
+
+  dim = size(mask);
+  d = zeros(dim);
+  d(round(0.35*dim(1)):round(0.5*dim(1)),round(0.25*dim(2)):round(0.65*dim(2))) = 1;
+%     [xi,yi] = ndgrid(-dim(1)/2:dim(1)/2-1,-dim(2)/2:dim(2)/2-1);
+%     xmi = round(mean(xi(logical(mask(:)))));
+%     ymi = round(mean(yi(logical(mask(:)))));
+%     d = circshift(d,[xmi ymi]); %Center the excitation pattern
+  d = ift2((hamming(dim(1))*hamming(dim(2))').^2.*ft2(d));%Smooth it a bit.
+  d = sind(15)*d;
+  dFreqDomain = fftshift(ifft2(d));
+  
+  ImageRes = 0.4; % cm
+  FDPixSize = 1./(dim*ImageRes); %cm
+  FDRadius = 0; %number of points. 
+  figure(11); imagesc([-FDPixSize(1)*dim(1)/2 FDPixSize(1)*dim(1)/2],...
+      [-FDPixSize(2)*dim(2)/2 FDPixSize(2)*dim(2)/2],abs(dFreqDomain)); 
+  title('2D FFT of the desired pattern'); 
+  xlabel('kx 1/cm'); ylabel('ky 1/cm');
+  TotalPwer = sum(abs(dFreqDomain(:)));
+  PowerPercentage = 0;
+  PowerThreshold = 0.9;
+  while PowerPercentage < PowerThreshold && FDRadius <= floor(min(dim)/2)
+      FDRadius = FDRadius + 1;
+      PartialKSpace = dFreqDomain((dim(1)/2-(FDRadius-1)):(dim(1)/2+FDRadius), ...
+          (dim(2)/2-(FDRadius-1)):(dim(2)/2+FDRadius));
+      PartialPower = sum(abs(PartialKSpace(:)));
+      PowerPercentage = PartialPower/TotalPwer;
+  end
+  fprintf('The power percentage with a %.4f cm(-1) by %.4f cm(-1) k-space is %.1f%%.\n', ...
+      FDRadius*(FDPixSize(1)), FDRadius*(FDPixSize(2)),100*PowerPercentage);   
+%% Get k-space trajectory
+deltax = 22/50; % spatial resolution of trajectory
 % Modified. Originally 15/50.
-FOVsubsamp = 45; % cm, xfov of spiral
+FOVsubsamp = 30; % cm, xfov of spiral
 densamp = 75; % duration of full density sampling (# of samples)
 dentrans = 75; % duration of transition from higher to lower
 nl = 1; % degree of undersampling outer part (any real #)
@@ -64,34 +93,12 @@ for ii = 2:size(gHzpercm,1)
     kTraj(ii,2) = kTraj(ii-1,2) + gHzpercm(ii,2);
 end
 kTraj = kTraj*dt; % to convert to 1/cm;  
-figure(11)
+figure(12)
 plot(kTraj(:,1),kTraj(:,2))
 xlabel('kx 1/cm')
 ylabel('ky 1/cm')
 title('k-space trajectory')
-%% Load excitation pattern and center it around the mask
-%   load OffRect.mat
-%   dim = size(mask);
-%   
-%   [xi,yi] = ndgrid(-dim(1)/2:dim(1)/2-1,-dim(2)/2:dim(2)/2-1);
-%   xmi = round(mean(xi(logical(mask(:)))));
-%   ymi = round(mean(yi(logical(mask(:)))));
-%   %d = circshift(d,[xmi ymi]); %Center the excitation pattern
-  dim = size(mask);
-  d = zeros(dim);
-  d(round(0.35*dim(1)):round(0.5*dim(1)),round(0.25*dim(2)):round(0.65*dim(2))) = 1;
-  d = ift2((hamming(dim(1))*hamming(dim(2))').^2.*ft2(d));%Smooth it a bit.
-  dFreqDomain = fftshift(ifft2(d));figure(12);imagesc(abs(dFreqDomain));
-  ImageRes = 0.4; % cm
-  FDPixSize = 1./(dim*ImageRes); %cm
-  FDRadius = 10; %number of points. 
-  TotalEnergy = sum(abs(dFreqDomain(:)));
-  PartialKSpace = dFreqDomain((dim(1)/2-(FDRadius-1)):(dim(1)/2+FDRadius), ...
-      (dim(2)/2-(FDRadius-1)):(dim(2)/2+FDRadius));
-  PartialEnergy = sum(abs(PartialKSpace(:)));
-  PowerPercentage = PartialEnergy/TotalEnergy;
-  fprintf('The power percentage with a k-space with size of %.4f cm(-1) by %.4f cm(-1) is %.1f%%.\n', ...
-      FDRadius*(FDPixSize(1)), FDRadius*(FDPixSize(2)),100*PowerPercentage);
+
  %% Build RF waveform roughness penalty matrix 
     roughbeta = 10^2.25;
     R = sqrt(roughbeta)*spdiags([-ones(Nt,1) ones(Nt,1)],[0 1],Nt,Nt) + ...
@@ -121,17 +128,17 @@ fprintf('Max RF = %f\n',max(abs(RFOut)))
   maxamp = max(abs([d(:);m(:)]));
   figure(14)
   subplot(221)
-  imagesc(imrotate(abs(d),90),[0 maxamp]);axis image;colorbar
+  imagesc(imrotate(flip(abs(d),1),-90),[0 maxamp]);axis image;colorbar
   title 'Desired pattern'
   subplot(222)
-  imagesc(imrotate(abs(m),90),[0 maxamp]);axis image;colorbar
+  imagesc(imrotate(flip(abs(m),1),-90),[0 maxamp]);axis image;colorbar
   title(sprintf('Final pattern'));
   subplot(223)
-  imagesc(imrotate(abs(magnetization.mxy),90),[0 maxamp]);axis image;colorbar
+  imagesc(imrotate(flip(abs(magnetization.mxy),1),-90),[0 maxamp]);axis image;colorbar
   title(sprintf('Bloch Simulation'));
   subplot(224)
-  imagesc(imrotate(abs(m)-abs(d),90),[0 maxamp]);axis image;colorbar
-  title(sprintf('Magnitude Error\nComplex NRMSE = %0.2f%%',MagErr*100));
+  imagesc(imrotate(flip(abs(m)-abs(d),1),-90),[0 maxamp]);axis image;colorbar
+  title(sprintf('Magnitude Error\nNRMSE = %0.2f%%',MagErr*100));
   fprintf('The slice position is %.4f mm.\n',10*Positions(1,3));%Positions in cm.
 
 %% Writing into text file
