492ebd6ce31e5c1599e011ba3e6076820b7b85c7
Yan <yan.tong@univ.ox.ac.uk>
2017-11-03 16:01:44 +0000

    Cleaned up some plotting steps.

--- tyRFShimming/main.m
+++ tyRFShimming/main.m
@@ -31,7 +31,7 @@
     
     %%  Constructing system matrix
     disp('Calculating system matrix...');
-    SysMatMode = 'Approximation';
+    SysMatMode = 'Full';
     if strcmp(SysMatMode,'Approximation');
         AFull = getAMatSimp(RFStruct,maskedMaps.b1SensMasked,maskedMaps.b0MapMasked); %Nv-(2*Nc) sys mtx, rad/V
     elseif strcmp(SysMatMode,'Full');
@@ -40,8 +40,7 @@
         tp = 10E-6 * ones(size(rfOn));
         DA = genAMatFull(tp,rfOn,gr,maskedMaps.b1SensMaskedHz,...
             maskedMaps.b0MapMasked,maskedMaps.posVox);
-     %  To take the RF pulse shape into account. Construct a
-     %  pseudo-diagonal matrix
+     %  To take the RF pulse shape into account. Construct a pseudo-diagonal matrix
         RFDiag = zeros(size(DA,2),8);
         RFSize = numel(RFStruct.RF_pulse);
         for iDx = 1:8
@@ -61,7 +60,7 @@
         [bVE(:,iDx),NRMSETmp,~,~] = runVE(AFull,param,maskedMaps);
         disp(NRMSETmp)
     end
-    disp('Variable-exchange optimization finished')
+    disp('Variable-exchange optimization complete')
     %%  Running RF shimming Step 2: Active-set method
     disp('Running RF shimming active-set optimization...')
     bAS = zeros(16,numel(tikhonovArray));
@@ -73,58 +72,53 @@
         [bAS(:,iDx),NRMSE(iDx),exitflag(iDx),output{iDx}] = runAS(bVE(:,iDx),RFStruct,maskedMaps,param,AFull);
     end
     [~, minIndex] = min(NRMSE);
-    disp('Active-set optimization finished')    
+    disp('Active-set optimization complete')    
     %%  Bloch Simulation
     disp('Running Bloch simulation...')
-    bmin = bVE(:,minIndex);
-    %bmin = bAS(1:8,minIndex).*exp(1i*bAS(9:16,minIndex));
-    RFToSim = bsxfun(@times, (bmin), repmat(RFStruct.RF_pulse,8,1));
-    RFToSim = RFToSim';
+    %bmin = bVE(:,minIndex);
+    bmin = bAS(1:8,minIndex).*exp(1i*bAS(9:16,minIndex));
+    RFToSim = bsxfun(@times,repmat(RFStruct.RF_pulse.',1,8),bmin.');    % Note the diff between .' and '
     GToSim = zeros(numel(RFToSim)/8,3);
     %GToSim(:,3) = 425.8*RFStruct.G_amp*ones(numel(RFToSim)/8,1);     %from mT/m to Hz/cm
     B1ToSim = ptxFMObj.getB1PerV('Hz','delete');
     magnetization = make_blochSim(RFToSim,B1ToSim,...
         maskedMaps.b0MapMasked,GToSim,10E-6,maskedMaps.posVox,maskedMaps.mask);
     disp('Bloch simulation complete')
-    %  Plotting
-% 
-%  figure(56)
-%   imagesc(maskedMaps.localiser(:,:,SliceIdx));
-%   title('Select Rect Regions to Plot')
-%   h2plot = imrect;
-%   wait(h2plot);
-%   plotMask = logical(createMask(h2plot));
-%   [rol,cow] = find(plotMask);
-%   plotRolArray = rol(1):rol(end);
-%   plotCowArray = cow(1):cow(end);   
-    %bTmp = bAS(1:8,1).*exp(1i*bAS(9:16,1));
+    %%  Plotting
+  if ~exist('plotRolArray','var')
+      figure(56)
+      imagesc(maskedMaps.localiser(:,:,SliceIdx));
+      title('Select Rect Regions to Plot')
+      h2plot = imrect;  wait(h2plot);
+      plotMask = logical(createMask(h2plot));
+      [rol,cow] = find(plotMask);
+      plotRolArray = rol(1):rol(end);
+      plotCowArray = cow(1):cow(end);     
+  end
+  
   mVec = AFull*(bmin);
   m = zeros(size(maskedMaps.mask));
   m(maskedMaps.mask) = mVec;
-  % Scaling
+  % Scaling (showing flip angle)
   m = rad2deg(m);
-  %mBloch = asind(abs(magnetization.mxy));
-  mBloch = magnetization.mxy;
+  mBloch = asind(magnetization.mxy);
   
-  figure(57)
-  PlotFALim = [10 25];
-  
-  subplot(1,3,1);imagesc(abs(mBloch(plotRolArray,plotCowArray,SliceIdx-1)));title(sprintf('Slice %d',SliceIdx-1));colorbar
-  subplot(1,3,2);imagesc(abs(mBloch(plotRolArray,plotCowArray,SliceIdx)));title(sprintf('Slice %d',SliceIdx));colorbar 
-  subplot(1,3,3);imagesc(abs(mBloch(plotRolArray,plotCowArray,SliceIdx+1)));title(sprintf('Slice %d',SliceIdx+1));colorbar
+  figure(57);PlotFALim = [10 25];  
+  subplot(1,3,1);imagesc(abs(mBloch(plotRolArray,plotCowArray,SliceIdx-1)),PlotFALim);title(sprintf('Slice %d',SliceIdx-1));colorbar
+  subplot(1,3,2);imagesc(abs(mBloch(plotRolArray,plotCowArray,SliceIdx)),PlotFALim);title(sprintf('Slice %d',SliceIdx));colorbar 
+  subplot(1,3,3);imagesc(abs(mBloch(plotRolArray,plotCowArray,SliceIdx+1)),PlotFALim);title(sprintf('Slice %d',SliceIdx+1));colorbar
    
   figure(58)
-  
   subplot(1,3,1);imagesc(abs(m(plotRolArray,plotCowArray,SliceIdx-1)),PlotFALim);title(sprintf('Slice %d',SliceIdx-1));colorbar
   subplot(1,3,2);imagesc(abs(m(plotRolArray,plotCowArray,SliceIdx)),PlotFALim);title(sprintf('Slice %d',SliceIdx));colorbar 
   subplot(1,3,3);imagesc(abs(m(plotRolArray,plotCowArray,SliceIdx+1)),PlotFALim);title(sprintf('Slice %d',SliceIdx+1));colorbar
   
-%   figure(59)
-%   subplot(1,2,1);imagesc(abs(m(plotRolArray,plotCowArray,SliceIdx)));title('Central Slice');colorbar
-%   subplot(1,2,2);imagesc(abs(abs(m(plotRolArray,plotCowArray,SliceIdx)) - deg2rad(param.targetFlipAngle)*abs(maskedMaps.mask(:,:,10))))
-%   title('Central Slice Error');colorbar
-%     
-    
+  figure(59)
+  subplot(1,2,1);imagesc(abs(m(plotRolArray,plotCowArray,SliceIdx)),PlotFALim);title('Central slice');colorbar
+  error = mBloch - param.targetFlipAngle*abs(maskedMaps.mask);
+  subplot(1,2,2);imagesc(abs(error(plotRolArray,plotCowArray,SliceIdx)),[0 2]);
+  title('Central Slice Error');colorbar
+
 %%
  b1map = ptxFMObj.getB1PerV('Hz','NaN');
  b1mapCP = squeeze(sum(b1map,4));
@@ -133,7 +127,7 @@
  %%
  subplot(4,2,1);imagesc(abs(b1map(:,:,10)))
  
- %%
+ %%     WTC debugging
  B1ToSim = ptxFMObj.getB1PerV('Hz','delete');
  
  DA = genAMatFull(600e-6,true,false,maskedMaps.b1SensMaskedHz,...
@@ -172,16 +166,3 @@
  end
  
  
- %%
-     RFToSim = bsxfun(@times,repmat(RFStruct.RF_pulse.',1,8),voltvec);
-    magnetization = make_blochSim(RFToSim,B1ToSim,... 
-        maskedMaps.b0MapMasked,zeros(size(RFToSim,1),3),10E-6,maskedMaps.posVox,maskedMaps.mask);
-    
-    
-    
-        magnetization = make_blochSim(RFToSim,B1ToSim,...
-        maskedMaps.b0MapMasked,GToSim,10E-6,maskedMaps.posVox,maskedMaps.mask);
-    
-    
-    B1ToSim = ptxFMObj.getB1PerV('Hz','delete');    %WTC
-     B1ToSim = ptxFMObj.getB1PerV('Hz','delete');   %TY
