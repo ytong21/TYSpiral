10025d6e51142344f8d2d91e69e7fb933e7d0049
Yan <yan.tong@univ.ox.ac.uk>
2017-11-05 21:09:06 +0000

    Added phase only optimization.

--- tyRFShimming/runPhaseOnly.m
+++ tyRFShimming/runPhaseOnly.m
@@ -1,7 +1,7 @@
-function bCP = runPhaseOnly(AFullCP,param,RFStruct)
+function bPhase = runPhaseOnly(AFull,param,RFStruct)
     maxV = 239; % Check where I got this. (WTC)
-    ub = [maxV;];
-    lb = [0];
+    ub = [maxV; ones(8,1)*inf];
+    lb = [0; -ones(8,1)*inf];
     
     
     optionsFMin = optimoptions(@fmincon);
@@ -16,12 +16,11 @@ function bCP = runPhaseOnly(AFullCP,param,RFStruct)
     nonlincon = @(x) TYpowerConstraints_AS_Global_OneSpoke(x,protectedModeConstraints,...
         param.TR,RFStruct.RF_pulse);
 
-    TargetFA = ones(size(AFullCP,1),1)*deg2rad(param.targetFlipAngle);
-    xInitial = (TargetFA')/(AFullCP');
-    FunHandle = @(x) -goodnessOfFit(abs(AFullCP*x),TargetFA,'NRMSE');
+    TargetFA = ones(size(AFull,1),1)*deg2rad(param.targetFlipAngle);
+    FunHandle = @(x) -goodnessOfFit(abs(AFull*(x(1)*exp(1i*x(2:9)))),TargetFA,'NRMSE');
     %VoltageArray = 10:5:80;
     %bSingle = zeros(size(VoltageArray));
     for iDx = 1:numel(VoltageArray)
-        [bCP,~,~,~] = fmincon(FunHandle,xInitial,[],[],[],[],lb.',ub.',nonlincon,optionsFMin);
+        [bPhase,~,~,~] = fmincon(FunHandle,xInitial,[],[],[],[],lb.',ub.',nonlincon,optionsFMin);
     end
 end
