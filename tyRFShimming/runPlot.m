cd6d6c8fec95cc7b32a7b99f440005a24734c3ff
Yan <yan.tong@univ.ox.ac.uk>
2017-11-07 11:05:26 +0000

    Added copying to the master and slave.

--- tyRFShimming/runPlot.m
+++ tyRFShimming/runPlot.m
@@ -14,12 +14,12 @@ figure(90)
 set(gcf,'color','w','InvertHardcopy','off')
 set(gcf,'units','centimeters','position',[4 4 40 20],'paperunits','centimeters','paperposition',[0 0 40 20])
 clf
-PlotFALim = [0 30]; FontSize = 8;   CLB = cell(7,1); 
-Figs = cell(3,3);
+PlotFALim = [10 30]; FontSize = 9;   CLB = cell(7,1);   CLBFontSize = 8;
+Figs = cell(4,3);
 
 Figs{2,1} = subplot(3,2,3);
 imagesc(ImgToPlot.b1CP(plotRolArray,plotCowArray,SliceIdx)'); title('B1 Map','FontSize',FontSize);axis off;
-CLB{1} = colorbar('FontSize',FontSize);
+CLB{1} = colorbar('FontSize',CLBFontSize);
 CLB{1}.YLabel.String = 'Hz';    CLB{1}.YLabel.Rotation = 0;     
 CLB{1}.YLabel.Position = [0.5 280 0];
 hold on;colormap(Figs{2,1},'gray')
@@ -29,7 +29,7 @@ nudge(Figs{2,1},[0 0.05 0 0])
 
 Figs{3,1} = subplot(3,2,5); 
 imagesc(ImgToPlot.b0(plotRolArray,plotCowArray,SliceIdx)',[-200 200]); title('B0 Map','FontSize',FontSize);axis off;hold on;
-CLB{2} = colorbar('FontSize',FontSize);
+CLB{2} = colorbar('FontSize',CLBFontSize);
 CLB{2}.YLabel.String = 'Hz/V';    CLB{2}.YLabel.Rotation = 0;     
 CLB{2}.YLabel.Position = [0.5 700 0];
 colormap(Figs{3,1},'gray')
@@ -60,14 +60,14 @@ ylabel('Phase Only','FontSize',FontSize,'FontWeight','bold');
 Figs{2,2}.YLabel.Visible = 'on';
 set(Figs{2,2},'Position',[Figs{2,2}.Position(1) Figs{2,2}.Position(2) Figs{2,2}.Position(3) Figs{2,2}.Position(4)]);
 colormap(Figs{2,2},'hot')
-nudge(Figs{2,2},[0 0.05 0 0])
+nudge(Figs{2,2},[0 0.05 0 0]);
 
 Figs{3,2} = subplot(3,4,11);
 imagesc(FullImage.AS(plotRolArray,plotCowArray,2)',PlotFALim);axis off;
 set(Figs{3,2},'Position',[Figs{3,2}.Position(1) Figs{3,2}.Position(2) Figs{3,2}.Position(3) Figs{3,2}.Position(4)]);
 ylabel('Full RF Shimming','FontSize',FontSize,'FontWeight','bold');
 Figs{3,2}.YLabel.Visible = 'on';
-CLB{6} = colorbar('southoutside','FontSize',FontSize);
+CLB{6} = colorbar('southoutside','FontSize',CLBFontSize);
 CLB{6}.YLabel.String = 'Degrees (°)';    CLB{6}.YLabel.Rotation = 0;   
 colormap(Figs{3,2},'hot')
 nudge(Figs{3,2},[0 0.1 0 0])
@@ -94,7 +94,7 @@ imagesc(DiffImage.AS(plotRolArray,plotCowArray,2)',PlotDiffLim);axis off
 set(Figs{3,3},'Position',[Figs{3,3}.Position(1)-HoriOffset Figs{3,3}.Position(2) ...
     Figs{3,3}.Position(3) Figs{3,3}.Position(4)]);
 CLB{7} = colorbar('southoutside');
-CLB{7} = colorbar('southoutside','FontSize',FontSize);
+CLB{7} = colorbar('southoutside','FontSize',CLBFontSize);
 colormap(Figs{3,3},'parula')
 nudge(Figs{3,3},[0 0.1 0 0])
 nudge(CLB{7},[0 0.02 0 0])
