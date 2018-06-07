de7cb0f64622cbb8ddbef1b4ff9b2c99f6127d35
Yan <yan.tong@univ.ox.ac.uk>
2018-06-07 17:44:01 +0100

    Added plot function to compare rf shimming and cp mode perfusion weighted images.

---
+++ tyRFShimming/PlotPerfusion.m
@@ -0,0 +1,21 @@
+function PlotPerfusion(dt,CPSeriesNumber,pTxSeriesNumber)
+
+matched_ptx = dt.search('target','instance','query',@(inst,ser,stu) ser.SeriesNumber==pTxSeriesNumber);
+matched_CP = dt.search('target','instance','query',@(inst,ser,stu) ser.SeriesNumber==CPSeriesNumber);
+if isempty(matched_ptx) || isempty(matched_CP)
+    disp('Please check series number. Not images found');
+else
+    CP_Img = Spectro.dicomImage({matched_CP(1).Filename});
+    pTx_Img = Spectro.dicomImage({matched_ptx(1).Filename});
+    AllPoints = nonzeros([pTx_Img.image(:);CP_Img.image(:)]);
+    CutOff = prctile(AllPoints,99);
+    figure(99)
+    set(gcf,'color','w','InvertHardcopy','off')
+    set(gcf,'units','centimeters','position',[4 4 40 20],'paperunits','centimeters','paperposition',[0 0 40 20])
+    clf
+    subplot(1,2,1)
+    imagesc(CP_Img.image',[0 CutOff]);colormap('gray');axis off;title('CP mode','FontSize',14)
+    subplot(1,2,2)
+    imagesc(pTx_Img.image',[0 CutOff]);colormap('gray');axis off;title('RF shimming','FontSize',14)
+   
+end
