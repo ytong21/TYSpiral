131c9421cd5e046fceceab5c6f576707ab1d8987
Yan <yan.tong@univ.ox.ac.uk>
2018-06-14 23:01:06 +0200

    changed the figures to white hyper intensity images.

--- tyRFShimming/PlotPerfusion.m
+++ tyRFShimming/PlotPerfusion.m
@@ -8,14 +8,16 @@ else
     CP_Img = Spectro.dicomImage({matched_CP(1).Filename});
     pTx_Img = Spectro.dicomImage({matched_ptx(1).Filename});
     AllPoints = nonzeros([pTx_Img.image(:);CP_Img.image(:)]);
-    CutOff = prctile(AllPoints,99);
+    CPtoPlot = (max(AllPoints) - CP_Img.image)';
+    pTxtoPlot = (max(AllPoints) - pTx_Img.image)';
+    CutOff = prctile(max(AllPoints)-AllPoints,99);
     figure(99)
     set(gcf,'color','w','InvertHardcopy','off')
     set(gcf,'units','centimeters','position',[4 4 40 20],'paperunits','centimeters','paperposition',[0 0 40 20])
     clf
     subplot(1,2,1)
-    imagesc(CP_Img.image',[0 CutOff]);colormap('gray');axis off;title('CP mode','FontSize',14)
+    imagesc(CPtoPlot,[0 CutOff]);colormap('gray');axis off;title('CP mode','FontSize',14)
     subplot(1,2,2)
-    imagesc(pTx_Img.image',[0 CutOff]);colormap('gray');axis off;title('RF shimming','FontSize',14)
+    imagesc(pTxtoPlot,[0 CutOff]);colormap('gray');axis off;title('RF shimming','FontSize',14)
    
 end
