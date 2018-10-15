718c4272b2eaafd3c113bb9f245250af407077a9
Yan <yan.tong@univ.ox.ac.uk>
2018-10-15 14:35:18 +0100

    Added pulse integral and unit energy as fields for RFStruct.

--- tyRFShimming/tyMakeGaussian.m
+++ tyRFShimming/tyMakeGaussian.m
@@ -20,6 +20,8 @@ RF_pulse = RF_pulse(1,:);
     RFOn(G_shape == 1) = true;
 
     OffResonance = linspace(-5000,5000,2000);
+    % In the dupas paper, FAr is the FA produced by a pulse whose peak
+    % amplutude is 1uT, which is 42.57Hz.
     RF_pulse_bloch = 42.57*RF_pulse;
     [mxss1,myss1_mt,~] =  bloch_CTR_Hz...
             (complex(real(RF_pulse_bloch),imag(RF_pulse_bloch)),zeros(numel(RF_pulse_bloch),3),...
@@ -34,6 +36,10 @@ DeltaF = fwhm(OffResonance,mxy);
 GyroRatioPerMT = 42.57e3; %Hz/T
 G_amp = (2*pi*DeltaF)/(2*pi*GyroRatioPerMT*SliceThicknessInMeter); %mT/m
 
+
+
 RFStruct = struct('RF_pulse',RF_pulse,'GradShape',G_shape*G_amp,...
     'RFOn',RFOn,'DurationInSec',DurationInSec,'DeltaF',DeltaF,'mxy',mxy,...
     'FArBloch',FArBloch,'G_amp',G_amp);
+RFStruct.pulseInt = trapz(RF_pulse)*dt*1e-6;
+RFStruct.unitEnergy = trapz(RF_pulse.^2/50)*dt*1e-6;
