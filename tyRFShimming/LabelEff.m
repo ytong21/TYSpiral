7f947b9d5ab561a3d70cae0dbdaa22fad4683b10
Yan <yan.tong@univ.ox.ac.uk>
2018-10-15 14:28:43 +0100

    Changed the pulse type to be gaussian.

--- tyRFShimming/LabelEff.m
+++ tyRFShimming/LabelEff.m
@@ -1,4 +1,4 @@
-function [Efficiency,FinalMag] = LabelEff(FAinDeg,RFStruct)
+function [Efficiency,FinalMag] = LabelEff(FAinDeg,RFStruct, RF_sep_sec)
 Efficiency = zeros(size(FAinDeg));
 FinalMag = zeros(size(FAinDeg));
     FAinRad = deg2rad(FAinDeg);
@@ -6,8 +6,8 @@ FinalMag = zeros(size(FAinDeg));
     RFAmpInmT = RFAmpInuT/1000;
     
     for iDx = 1:numel(FAinRad)
-        [M, ~, ~, ~, ~] = test_regPCASL_seq_meanGz(0.3, 0.1, 0, 0.8, 6, 'hanning', ...
-            [], RFAmpInmT(iDx), 600e-6, 1000e-6, [0 0], [1 1], 1, [0 0], 20e-6, inf, 0.3, 0);
+        [M, ~, ~, ~, ~] = test_regPCASL_seq_meanGz(0.3, 0.1, 0, 0.8, 6, 'gaussian_dsv', ...
+            [], RFAmpInmT(iDx), RFStruct.DurationInSec, RF_sep_sec, [0 0], [1 1], 1, [0 0], 20e-6, inf, 0.3, 0);
         FinalMag(iDx) = M(3,end);
         Efficiency(iDx) = (1-FinalMag(iDx))/2;
     end
